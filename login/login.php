278ab4683114fbf6bed1dc9c39bed6e09350222f
KadenHD <71968563+KadenHD@users.noreply.github.com>
2020-12-14 13:53:56 +0100

    Add files via upload

---
+++ login/login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+        <link rel="stylesheet" href="./style/login.css">
+    </head>
+
+    <body>
+            <form method="post">
+            
+                <div class="email">
+                    <h3>Email</h3>
+                    <input for="email" id="email" type="email" name="email">
+                </div>
+                
+
+                <div class="password">
+                    <h3>Mot de passe</h3>
+                    <input for="password" id="password" type="password" name="password">
+                </div>
+                
+
+                <button class="button" id="button" type="submit" name="button">Connecter</button>
+
+            </form>
+    </body>
+
+</html>
+
+<!------------------------------- Connecte à la bdd  --------------------------------------------->
+<div class="php">
+<?php 
+try {
+    $bdd=new PDO ('mysql:host=localhost;dbname=gestion_hotel;charset=UTF8', 'root','');
+}
+
+catch(Exception $e) {
+    die('Erreur :' .$e->getMessage());
+}
+?>
+<!------------------------------------------------------------------------------------------------>
+
+<!------------------------------------------ Tentative de connexion  -------------------------------------------------------------------------->
+<?php
+if ($_POST) {
+	$request = $bdd ->prepare('SELECT * FROM user WHERE email = ? AND password = ?');
+	$request->execute(array(htmlspecialchars($_POST['email']), htmlspecialchars($_POST['password'])));
+	$user = $request->fetch(PDO::FETCH_ASSOC);//Compare
+
+	    //Connecte car similaire à la bdd
+        if(!empty($user)){ 
+            session_start();
+            $_SESSION['id_user'] = $user['id'];
+            $_SESSION['email'] = $user['email'];
+            header('location:index.php?error=none');
+        } 
+        
+    else {
+        echo "Email ou Mot de passe incorrect";
+    }
+}
+?>
+</div>
+<!--------------------------------------------------------------------------------------------------------------------------------------------->
