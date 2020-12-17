96dde1864ba2a8455cc1bd8b85adc44c8f284d8b
Pyxah <73198262+Pyxah@users.noreply.github.com>
2020-12-17 14:26:56 +0100

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,31 +1,53 @@
 <!DOCTYPE html>
-<html>
- <head>
-   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-   <link rel="shortcut icon" type="image/ico" href="img/favicon.gif" />
-   <link rel="stylesheet" type="text/css" href="./style/formulaire.css" />
-   <title>Hotel & Resort</title>
- </head>
- <body>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Formulaire</title>
+        <link rel="stylesheet" href="./style/formulaire.css">
+    </head>
+    <body>
 
- <div class="header">
-    <h1 id="head-title">Hotel & Resort</h1>
-    <ul id="boutton-margin">
-        <li><a href="#chambres">Chambres</a></li>
-        <li><a href="#personnel">Personnel</a></li>
-        <li id="déconnexion"><a href="logout.php">Déconnexion</a></li>
-    </ul>
-</div>
-
-<?php
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=gestion_hotel;charset=utf8', 'root', 'root');   
-	array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+<!------------------------------- Connecte à la bdd  --------------------------------------------->
+<div class="php">
+<?php 
+try {
+    $bdd=new PDO ('mysql:host=localhost;dbname=gestion_hotel;charset=UTF8', 'root','root');
 }
-catch(Exception $e)
-{
-	die('Erreur :'.$e->getMessage() );
+
+catch(Exception $e) {
+    die('Erreur :' .$e->getMessage());
 }
 ?>
+<!------------------------------------------------------------------------------------------------>
 
+<!--------------------------------------- Ajout employé ---------------------------------------------->
+
+<h2>Ajout d'un collaborateur</h1>
+
+      <form action="" method="post"> 
+        
+        <div class="formulaire">
+          <label for="nom">Nom</label> 
+          <input id="nom" type="text" name="nom">
+        
+          <label for="prenom">Prénom</label> 
+          <input id="prenom" type="text" name="prenom">
+        </div>
+
+        <button class="button" id="button" type="submit" name="button">Enregistrer</button>
+      </form>
+    </body>
+</html>
+<!---------------------------------------------------------------------------------------------------->
+
+<!-------------------------------------- Création de compte  ----------------------------------------------->
+<?php
+    //Fonction de création de compte
+    if ($_POST) { 
+            $request = $bdd->prepare('INSERT INTO employee (nom, prenom) VALUES (?, ?)');
+            $request->execute(array($_POST['nom'], $_POST['prenom']));
+            header('location:personnel.php?error=none');
+    }
+?>
+</div>
+<!----------------------------------------------------------------------------------------------------------->
