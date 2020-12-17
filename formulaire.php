964726806063c716481787e25dfeb9a75bc175b9
KadenHD <71968563+KadenHD@users.noreply.github.com>
2020-12-17 17:07:33 +0100

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,49 +1,73 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<link href="./formulaire.css" rel="stylesheet" type="text/css">
-    <link rel="icon" type="image/png" href="logo.png" />
-</head>
- <body>
-
- <div class="header">
-    <h1 id="head-title">Hotel & Resort</h1>
-    <ul id="buttonheader-margin">
-        <li><a href="index.php">Chambres</a></li>
-        <li><a href="personnel.php">Personnel</a></li>
-        <li id="déconnexion"><a href="logout.php">Déconnexion</a></li>
-    </ul>
-</div>        
-        
-<!------------------------------- Connecte à la bdd  --------------------------------------------->
-<div class="php">
-<?php 
-try {
-    $bdd=new PDO ('mysql:host=localhost;dbname=gestion_hotel;charset=UTF8', 'root','');
-}
-
-catch(Exception $e) {
-    die('Erreur :' .$e->getMessage());
+<!-------- Redirige sur le login si non connecté -------->
+<?php
+session_start();
+if(!isset($_SESSION['id_user'])) {
+    header('location:login.php?error=none');
 }
 ?>
-<!------------------------------------------------------------------------------------------------>
+<!------------------------------------------------------->
 
-<!--------------------------------------- Ajout employé ---------------------------------------------->
+<!DOCTYPE html>
 
-<h2>Ajout d'un collaborateur</h2>
+<html lang="fr">
 
-      <form method="post"> 
-        
-        <div class="Nom">
-          <h3>Nom</h3><input id="nom" type="text" name="nom"><h3>Prénom</h3><input id="prenom" type="text" name="prenom"></div> 
+    <head>
+        <meta charset="utf-8">
+        <title>Formulaire</title>
+        <link href="./style/formulaire.css" rel="stylesheet" type="text/css">
+        <link rel="icon" type="image/png" href="logo.png" />
+    </head>
 
-        <button class="button" id="button" type="submit" name="button">Enregistrer</button>
-      </form>
+    <body>
 
-</body>
+        <div class="header">
+
+            <h1 id="head-title">Hotel & Resort</h1>
+
+            <ul id="buttonheader-margin">
+                <li><a href="index.php">Chambres</a></li>
+                <li><a href="personnel.php">Personnel</a></li>
+                <li id="déconnexion"><a href="logout.php">Déconnexion</a></li>
+            </ul>
+
+        </div>        
+
+        <h2>Ajout d'un collaborateur</h2>
+
+        <form method="post"> 
+            
+            <div class="box">
+
+                <div class="box1">
+                <h3>Nom</h3><input id="nom" type="text" name="nom">
+                </div> 
+
+                <div class="box2">
+                <h3>Prénom</h3><input id="prenom" type="text" name="prenom">
+                </div> 
+
+            </div> 
+
+            <button class="button" id="button" type="submit" name="button">Enregistrer</button>
+
+        </form>
+
+    </body>
+
 </html>
-<!---------------------------------------------------------------------------------------------------->
 
+<!------------------------------- Connecte à la bdd  --------------------------------------------->
+<div class="php">
+    <?php 
+    try {
+        $bdd=new PDO ('mysql:host=localhost;dbname=gestion_hotel;charset=UTF8', 'root','');
+    }
+
+    catch(Exception $e) {
+        die('Erreur :' .$e->getMessage());
+    }
+    ?>
+    <!------------------------------------------------------------------------------------------------>
 <!-------------------------------------- Création de compte  ----------------------------------------------->
 <?php
     //Fonction de création de compte
