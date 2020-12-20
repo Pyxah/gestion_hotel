cc09ea3e9592e5efbac8f4e22e23942594a79a8b
Fossard-Thomas <72501555+Fossard-Thomas@users.noreply.github.com>
2020-12-20 16:43:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,17 @@
    <link rel="shortcut icon" type="image/ico" href="img/favicon.gif" />
    <link rel="stylesheet" type="text/css" href="./style/index.css" />
    <title>Hotel & Resort</title>
+   <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+			integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+			crossorigin="anonymous"></script>
  </head>
  <body>
 
  <div class="header">
     <h1 id="head-title">Hotel & Resort</h1>
     <ul id="boutton-margin">
-        <li><a href="index.php">Chambres</a></li>
-        <li><a href="personnel.php">Personnel</a></li>
+        <li><a href="#chambres">Chambres</a></li>
+        <li><a href="#personnel">Personnel</a></li>
         <li id="déconnexion"><a href="logout.php">Déconnexion</a></li>
     </ul>
 </div>
@@ -49,26 +52,83 @@ if(!isset($_SESSION['id_user'])) {
         {
         ?>
         <div class="case_chambre">
-            <?php
-            echo '<p>Chambre '. $data['numero'] .'</br>Etage '.$data['etage'].'</br></br></p>';
-            if ($data['statut'] == 'libre') {
-                echo '<p class="libre">Libre</p>';
-            }
+            <div class="chambre active" style="z-index :20">
+                <?php
+                echo '<p>Chambre '. $data['numero'] .'</br>Etage '.$data['etage'].'</br></br></p>';
+                if ($data['statut'] == 'libre') {
+                    echo '<p class="libre">Libre</p>';
 
-            elseif ($data['statut'] == 'occupe') {
-                echo '<p class="occupe">Occupé</p>';
-            }
+                }
 
-            else {
-                echo '<p class="nettoyage">A nettoyer</p>';
-            }
-            
-            ?>
+                elseif ($data['statut'] == 'occupe') {
+                    echo '<p class="occupe">Occupé</p>';
+                }
+
+                else {
+                    echo '<p class="nettoyage">A nettoyer</p>';
+                }
+                
+                ?>
+            </div>
+
+            <div class="dropdown dropdown2">
+                <select id="status" class="input100" type="select" name="role" placeholder="Choose your Role" required>
+                    <option value="1">Libre</option>
+                    <option value="2">Occupé</option>
+                    <option value="3">A nettoyer</option>
+                </select>
+
+                <select id="employe" class="input00" type="select" name="role1" placeholder="Choose your Role" required style="display: none;">
+                    <?php
+                    $getAllEmployee = $bdd->query('SELECT nom, prenom, chambre, id_employee from employee where chambre is null');
+                    foreach ($getAllEmployee->fetchAll() as $data) {
+                    echo '<option value="'.$data['id_employee'].'">'.$data['nom'].' '.$data['prenom'].'</option>'; }
+                    ?>
+                </select>
+            </div>
         </div>
         <?php
         }
     ?>
 </div>
+
+
+<script>
+    $(document).ready(function() {
+
+        $('.dropdown').hide()
+        let display = false
+
+        $('#employe').hide()
+        $('#status').change(function() {
+            if ($(this).val() == 3) {
+                $('#employe').show()
+            } else {
+                $('#employe').hide()
+            }
+        })
+
+        $('.chambre').click(function() {
+            
+            if (display == false) {
+                display = true
+                $(this).parent().find('.dropdown').show()
+                $('.chambre').css('pointer-events', 'none')
+                $(this).css('pointer-events', 'auto')
+                $(this).css('z-index', '10')
+            } else {
+                display = false
+                $(this).parent().find('.dropdown').hide()
+                $(this).find('.dropdown').hide()
+                $('.chambre').css('pointer-events', 'auto')
+                $(this).css('z-index', '0')
+            }
+        })
+    })
+</script>
+
+<!--<script src="script.js"></script>
+<script src="jquery.js"></script>-->
 
  </body>
 </html>
